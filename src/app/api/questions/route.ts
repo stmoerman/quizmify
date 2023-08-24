c75aa2a1dc8fb8fb018ce418b7471f1552877f25
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-24 22:27:15 +0200

    Updated question generation API to make sure it always returns questions

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -21,33 +21,44 @@ export const POST = async (req: Request, res: Response) => {
     // }
     const body = await req.json();
     const { amount, topic, type } = quizCreationSchema.parse(body);
-    let questions: any;
-    if (type === "open_ended") {
-      questions = await strict_output(
-        "You are a helpful AI that is able to generate a pair of questions and answers, the length of the answer should not exceed 15 words, store all the pairs of answers and questions in a JSON array",
-        new Array(amount).fill(
-          `You are to generate a random hard open-ended question about ${topic}`
-        ),
-        {
-          question: "question",
-          answer: "answer with max length of 15 words",
-        }
-      );
-    } else if (type === "mcq") {
-      questions = await strict_output(
-        "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not exceed 15 words",
-        new Array(amount).fill(
-          `You are to generate a random mcq about ${topic}`
-        ),
-        {
-          question: "question",
-          answer: "answer with max length of 15 words",
-          option1: "1st option with max length of 15 words",
-          option2: "2nd option with max length of 15 words",
-          option3: "3rd option with max length of 15 words",
-        }
-      );
+
+    let questions: any[] = [];
+
+    while (questions.length < amount) {
+      let generatedQuestions: any[] = []; // Initialize with an empty array
+
+      if (type === "open_ended") {
+        generatedQuestions = await strict_output(
+          "You are a helpful AI that is able to generate a pair of questions and answers, the length of the answer should not exceed 15 words, store all the pairs of answers and questions in a JSON array",
+          new Array(amount - questions.length).fill(
+            `You are to generate a random hard open-ended question about ${topic}`
+          ),
+          {
+            question: "question",
+            answer: "answer with max length of 15 words",
+          }
+        );
+      } else if (type === "mcq") {
+        generatedQuestions = await strict_output(
+          "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not exceed 15 words",
+          new Array(amount - questions.length).fill(
+            `You are to generate a random mcq about ${topic}`
+          ),
+          {
+            question: "question",
+            answer: "answer with max length of 15 words",
+            option1: "1st option with max length of 15 words",
+            option2: "2nd option with max length of 15 words",
+            option3: "3rd option with max length of 15 words",
+          }
+        );
+      }
+
+      questions = questions.concat(generatedQuestions);
     }
+
+    questions = questions.slice(0, amount); // Ensure the final list matches the specified amount
+
     return NextResponse.json({ questions }, { status: 200 });
   } catch (error) {
     if (error instanceof ZodError) {
