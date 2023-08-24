1e3bf9741e5c30cecd6b3334903e3991153be835
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-24 13:41:31 +0200

    fix: removed session logging from navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,6 @@ type Props = {};
 
 const Navbar = async (props: Props) => {
   const session = await getAuthSession();
-  console.log(session?.user);
   return (
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
